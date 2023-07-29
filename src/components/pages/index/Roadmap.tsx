7318c76dbdc9abd4ef181fbed554dd4687843a03
anasdeyra <anasdoura607@gmail.com>
2023-07-29 23:02:58 +0100

    navbar update

--- src/components/pages/index/Roadmap.tsx
+++ src/components/pages/index/Roadmap.tsx
@@ -6,6 +6,7 @@ import { motion } from "framer-motion";
 export default function Roadmap() {
   return (
     <div
+      id="roadmap"
       style={{
         backgroundImage: "url('/roadmap.png')",
         backgroundSize: "cover",
